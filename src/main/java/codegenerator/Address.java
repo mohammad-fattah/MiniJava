842a838bfc257ce134ca738e8392dd625c7a2caf
mohammad-fattah <m.p.fattahpour@gamail.com>
2021-12-31 12:52:20 -0400

    adding default to Switch case in Address.java

--- src/main/java/codegenerator/Address.java
+++ src/main/java/codegenerator/Address.java
@@ -19,15 +19,17 @@ public class Address {
         this.Type = TypeAddress.Direct;
         this.varType = varType;
     }
-    public String toString(){
-        switch (Type){
+    public String toString() {
+        switch (Type) {
             case Direct:
-                return num+"";
+                return num + "";
             case Indirect:
-                return "@"+num;
+                return "@" + num;
             case Imidiate:
-                return "#"+num;
+                return "#" + num;
+            default:
+                return num + "";
+
         }
-        return num+"";
     }
 }
